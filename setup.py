74838370bd8640b0fe93b61cf857d25149a116c6
Jaewhoon Cho <aerospace105@gmail.com>
2022-05-15 20:25:52 +0900

    추가 수정

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="markdown",
+    name="markdownx",
     version="0.0.1",
     author="Jaewhoon, Cho",
     author_email="aerospace105@gmail.com",
